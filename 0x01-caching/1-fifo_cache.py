7d23a0ae20d4018b818364d3b85aad6dd66dd6a4
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-26 11:53:45 +0000

    fix to always delete from first key

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -11,25 +11,20 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """initializes the class"""
         super().__init__()
-        self.oldest = None
-        self.next_oldest = None
+        self.oldest = 0
 
     def put(self, key, item):
         """adds item to the cache"""
         if key is None or item is None:
             return
         if len(list(self.cache_data.keys())) == 0:
-            self.oldest = key
             self.cache_data[key] = item
         elif len(list(self.cache_data.keys())) == self.MAX_ITEMS:
             if key in self.cache_data:
                 del self.cache_data[key]
                 self.cache_data[key] = item
                 return
-            cache_keys = list(self.cache_data.keys())
-            delete_candidate = self.oldest
-            index = (cache_keys.index(delete_candidate) + 1) % self.MAX_ITEMS
-            self.oldest = cache_keys[index]
+            delete_candidate = list(self.cache_data.keys())[self.oldest]
             del self.cache_data[delete_candidate]
             print(f"DISCARD: {delete_candidate}")
             self.cache_data[key] = item
