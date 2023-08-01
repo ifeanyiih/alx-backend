eb2542b05d242e748918190db615d75ca419b067
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-01 22:19:05 +0000

    modify task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,10 +4,11 @@ Instantiate a Babel object.
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
+from typing import Mapping, Dict, List, Union
 
-app = Flask(__name__)
-babel = Babel(app)
-users = {
+app: Flask = Flask(__name__)
+babel: Babel = Babel(app)
+users: Mapping = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
@@ -15,7 +16,7 @@ users = {
 }
 
 
-def get_user(ID: int) -> dict:
+def get_user(ID: int) -> Union[Dict, None]:
     """Returns a user dictionary
     or None"""
     if users.get(ID):
@@ -27,16 +28,16 @@ class Config(object):
     """Configuration class
     Class will serve as configuration for
     the flask app object"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
 
 
 app.config.from_object(Config)
 
 
 @app.before_request
-def before_request():
+def before_request() -> None:
     """Function to be executed before
     any request"""
     login_as = request.args.get('login_as')
@@ -49,13 +50,21 @@ def get_locale() -> str:
     """determines the best match
     with supported languages"""
     locale = request.args.get('locale')
+    login_as = request.args.get('login_as')
+    user = None
+    if login_as:
+        user = get_user(int(login_as))
     if locale and locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    if user and user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+    if request.accept_languages:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return app.config['BABEL_DEFAULT_LOCALE']
 
 
 @app.route("/")
 def home() -> str:
     """home route
     This is the root of the application"""
-    return render_template('5-index.html')
+    return render_template('6-index.html')
