d0dc791eae05290884f0f8f7c0eac3fcc748aa4c
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-01 14:37:43 +0000

    complete task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Instantiate a Babel object.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config['BABEL_DEFAULT_LOCALE'] = Config.BABEL_DEFAULT_LOCALE
+app.config['BABEL_DEFAULT_TIMEZONE'] = Config.BABEL_DEFAULT_TIMEZONE
+
+@app.route("/")
+def home() -> str:
+    """home route"""
+    return render_template('1-index.html')
