435e61419e4ccb8d66dc530762a0ead53d77442e
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-01 15:32:48 +0000

    add docs

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -23,6 +23,8 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale() -> str:
+    """determines the best match
+    with supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
