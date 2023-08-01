f2734f65bcb23976a0ae6464aa7db29f1cc620c4
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-01 14:47:05 +0000

    more doc

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+"""Setup a basic Flask app with template.
 Instantiate a Babel object.
 """
 from flask import Flask, render_template
@@ -9,8 +9,10 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    """Configuration class"""
+class Config():
+    """Configuration class
+    Class will serve as configuration for
+    the flask app object"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
