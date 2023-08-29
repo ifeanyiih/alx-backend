0cfb21fd4046d4b2ecbbdbb496df1ba3a8c8ba33
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-29 11:29:05 +0000

    Complete: task 9

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,20 @@
+const kue = require('kue');
+
+const blacklist = ['4153518780', '4153518781'];
+
+function sendNotification(phone, message, job, done) {
+    job.progress(0, 100);
+    if (blacklist.includes(phone)) {
+       done(new Error(`Phone number ${phone} is blacklisted`));
+    }
+    else {
+        job.progress(50, 100);
+        console.log(`Sending notification to ${phone}, with message: ${message}`);
+        done();
+    }
+}
+
+const processor = kue.createQueue();
+processor.process('phone', 2, (job, done) => {
+    sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
